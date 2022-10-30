4dcc254589d27c6b33ad08c75f528a1eb77a0a7e
Hector Vazquez <hvasquezdev@gmail.com>
2022-10-30 20:22:50 -0300

    :sparkles:(edit todo page): add basic behavior to edit todo page

--- src/routes/EditTodoPage.js
+++ src/routes/EditTodoPage.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { TodoForm } from "../components/TodoForm";
+import { useTodos } from "../hooks/useTodos";
 
 function EditTodoPage() {
-  return <h1>Edit Todo</h1>;
+  const { getTodoItemById, loading } = useTodos();
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  const [todo, setTodo] = useState({});
+
+  React.useEffect(() => {
+    if (id && !loading) {
+      setTodo(getTodoItemById(id));
+    }
+    //eslint-disable-next-line
+  }, [loading]);
+
+  const navigateToHome = () => {
+    navigate("/");
+  }
+
+  return (
+    <div className="editTodoPage">
+      <TodoForm
+        formLabel="Edita el TODO seleccionado"
+        submitLabel="Guardar cambios"
+        defaultValue={todo?.text}
+        submitTodo={(val) => console.log("Editar todo", val)}
+        handleClose={navigateToHome}
+      />
+    </div>
+  );
 }
 
 export { EditTodoPage };
